32a2ab5699cd98953f600239465ac8f230873b9e
sankshitpandoh <sankshit02pandoh.sp@gmail.com>
2020-04-15 20:22:34 +0530

    mouse wheel H scroll added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,28 @@
+(function() {
+    function scrollHorizontally(e) {
+        e = window.event || e;
+        var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+        document.querySelector(".wrapper").scrollLeft -= (delta*40); // Multiplied by 40
+        e.preventDefault();
+    }
+    if (document.querySelector(".wrapper").addEventListener) {
+        // IE9, Chrome, Safari, Opera
+        document.querySelector(".wrapper").addEventListener("mousewheel", scrollHorizontally, false);
+        // Firefox
+        document.querySelector(".wrapper").addEventListener("DOMMouseScroll", scrollHorizontally, false);
+    } else {
+        // IE 6/7/8
+        document.querySelector(".wrapper").attachEvent("onmousewheel", scrollHorizontally);
+    }
+})();
+
+
 let elem = document.querySelector('.grid');
 let msnry = new Masonry( elem, {
     // options
     itemSelector: '.grid-item',
     columnWidth: 40,
-    gutter: 5   
+    // gutter: 5   
   });
 
 
@@ -13,6 +32,5 @@ let msnry = new Masonry( elem, {
         document.querySelector('.wrapper');
         scrollHor.addEventListener('scroll', event =>{
             let scrolled = (scrollHor.scrollLeft / (scrollHor.scrollWidth - scrollHor.clientWidth) ) * 100
-        console.log("hi")
             document.getElementById("progress-scroll-bar").style.width = scrolled + "%"
         })
